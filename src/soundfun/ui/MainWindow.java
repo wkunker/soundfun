4adfdca9b2580089373fde8eb93c6e5a76218348
Denny Kunker <wkunker@gmail.com>
2013-03-22 19:07:38 -0400

    UI improvements: MainWindow in soundfun.ui now relies on MigLayout. Windows now scale "more correctly."

--- src/soundfun/ui/MainWindow.java
+++ src/soundfun/ui/MainWindow.java
@@ -2,10 +2,7 @@ package soundfun.ui;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-
-import javax.swing.Box;
+import net.miginfocom.swing.MigLayout;
 
 /*
  * This class is NOT to be considered part of the API at this stage.
@@ -13,12 +10,12 @@ import javax.swing.Box;
  */
 public class MainWindow {	
 	private Frame mMainWindowFrame = new Frame();
-	private Panel mTopPanel = new Panel();
-	private Panel mBottomPanel = new Panel();
+	private Panel mTopPanel = new Panel(new MigLayout("fill"));
+	private Panel mBottomPanel = new Panel(new MigLayout("fill"));
 	
 	// The three visible panels as the user knows them.
-	private Panel mButtonPanel = new Panel();
-	private Panel mActionPanel = new Panel();
+	private Panel mButtonPanel = new Panel(new MigLayout("fill"));
+	private Panel mActionPanel = new Panel(new MigLayout("fill"));
 	private Panel mActionDetailsPanel = new Panel();
 	
 	/*
@@ -43,61 +40,31 @@ public class MainWindow {
 		return mButtonListPanel;
 	}
 	
-	/*
-	 * For internal use by soundfun.ui.
-	 */
-	void _updateSizes() {
-		mActionPanel.setSize(
-        		new Dimension(
-        				mMainWindowFrame.getWidth() / 2,
-        				mMainWindowFrame.getHeight() / 2
-        		));
-		
-		mButtonPanel.setSize(
-        		new Dimension(
-        				mMainWindowFrame.getWidth() / 2,
-        				mMainWindowFrame.getHeight() / 2
-        		));
-		
-		mButtonScrollPane.setSize(
-        		new Dimension(
-        				mMainWindowFrame.getWidth() / 2,
-        				mMainWindowFrame.getHeight() / 2
-        		));
-		
-		mActionScrollPane.setSize(
-        		new Dimension(
-        				mMainWindowFrame.getWidth() / 2,
-        				mMainWindowFrame.getHeight() / 2
-        		));
-		
-		mTopPanel.setSize(new Dimension(mMainWindowFrame.getSize().width, mTopPanel.getSize().height));
-		
-		mMainWindowFrame.validate();
-	}
-	
 	/**
      * Creates new form MainFrame
      */
     public MainWindow() {
-    	mMainWindowFrame.getContentPane().setLayout(new javax.swing.BoxLayout(mMainWindowFrame.getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+    	///mMainWindowFrame.getContentPane().setLayout(new javax.swing.BoxLayout(mMainWindowFrame.getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+    	mMainWindowFrame.getContentPane().setLayout(new MigLayout("fill"));
     	mMainWindowFrame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     	
     	/*
     	 * Define the outer-most layout. This layout
     	 * splits the frames along the x-axis.
     	 */
-    	GridBagConstraints frameConstraints = new GridBagConstraints();
-    	frameConstraints.fill = GridBagConstraints.HORIZONTAL;
-    	frameConstraints.gridx = 0;
-    	frameConstraints.gridy = 0;
-    	frameConstraints.weightx = 1.0;
-    	mMainWindowFrame.add(mTopPanel, frameConstraints);
+    	///GridBagConstraints frameConstraints = new GridBagConstraints();
+    	///frameConstraints.fill = GridBagConstraints.HORIZONTAL;
+    	///frameConstraints.gridx = 0;
+    	///frameConstraints.gridy = 0;
+    	///frameConstraints.weightx = 1.0;
+    	///mMainWindowFrame.add(mTopPanel, frameConstraints);
+    	mMainWindowFrame.add(mTopPanel, "w 100%, wrap, grow");
     	
-    	frameConstraints.fill = GridBagConstraints.HORIZONTAL;
-    	frameConstraints.gridx = 0;
-    	frameConstraints.gridy = 1;
-    	mMainWindowFrame.add(mBottomPanel, frameConstraints);
+    	///frameConstraints.fill = GridBagConstraints.HORIZONTAL;
+    	///frameConstraints.gridx = 0;
+    	///frameConstraints.gridy = 1;
+    	///mMainWindowFrame.add(mBottomPanel, frameConstraints);
+    	mMainWindowFrame.add(mBottomPanel, "w 100%, grow");
     	
     	/*
     	 * Arrange the panels for the top panel.
@@ -105,24 +72,24 @@ public class MainWindow {
     	 * panel that corresponds with the device,
     	 * and the action selection list box.
     	 */
-    	FlowLayout layout = new FlowLayout();
-    	mTopPanel.setLayout(layout);
-    	layout.setAlignment(FlowLayout.CENTER);
-    	layout.layoutContainer(mTopPanel);
-    	mTopPanel.add(mButtonPanel);
+    	///FlowLayout layout = new FlowLayout();
+    	///mTopPanel.setLayout(layout);
+    	///layout.setAlignment(FlowLayout.CENTER);
+    	///layout.layoutContainer(mTopPanel);
+    	mTopPanel.add(mButtonPanel, "w 50%, h 50%, grow");
     	
-    	mTopPanel.add(Box.createHorizontalGlue());
+    	///mTopPanel.add(Box.createHorizontalGlue());
     	
-    	layout.setAlignment(FlowLayout.CENTER);
-    	layout.layoutContainer(mTopPanel);
-    	mTopPanel.add(mActionPanel);
+    	///layout.setAlignment(FlowLayout.CENTER);
+    	///layout.layoutContainer(mTopPanel);
+    	mTopPanel.add(mActionPanel, "w 50%, h 50%, grow");
     	
     	/*
     	 * Attach the action details panel, which is normally
     	 * manipulated by whichever plugin is currently selected.
     	 * mActionDetailsPanel should fill mBottomPanel.
     	 */
-    	mBottomPanel.add(mActionDetailsPanel);
+    	mBottomPanel.add(mActionDetailsPanel, "w 100%, h 100%, grow");
     	
     	
     	mButtonPanel.add(mButtonListPanel);
@@ -140,7 +107,7 @@ public class MainWindow {
     	 */
     	mActionListPanel.getList().setModel(mActionListModel);
         mActionScrollPane.setViewportView(mActionListPanel.getList());
-        mActionPanel.add(mActionScrollPane);
+        mActionPanel.add(mActionScrollPane, "w 100%, h 100%");
         mActionPanel.add(mActionListPanel);
         
         /*
@@ -148,7 +115,7 @@ public class MainWindow {
          */
         mButtonListPanel.getList().setModel(mButtonListModel);
         mButtonScrollPane.setViewportView(mButtonListPanel.getList());
-        mButtonPanel.add(mButtonScrollPane);
+        mButtonPanel.add(mButtonScrollPane, "w 100%, h 100%");
         mButtonPanel.add(mButtonListPanel);
         
         /****************************
@@ -161,26 +128,21 @@ public class MainWindow {
         		mMainWindowFrame.getSize().height / 2 - 50
         		);
         
-        Dimension halfSizeY = new Dimension(
-        		mMainWindowFrame.getSize().width,
-        		mMainWindowFrame.getSize().height / 2
-        		);
+        ///mBottomPanel.setMinimumSize(halfSizeY);
+        ///mBottomPanel.setPreferredSize(halfSizeY);
+       /// mBottomPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
+        ///                                  Short.MAX_VALUE));
         
-        mBottomPanel.setMinimumSize(halfSizeY);
-        mBottomPanel.setPreferredSize(halfSizeY);
-        mBottomPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
-                                          Short.MAX_VALUE));
+        ///mActionDetailsPanel.setMinimumSize(halfSizeY);
+        ///mActionDetailsPanel.setPreferredSize(halfSizeY);
+        ///mActionDetailsPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
+        ///                                  Short.MAX_VALUE));
         
-        mActionDetailsPanel.setMinimumSize(halfSizeY);
-        mActionDetailsPanel.setPreferredSize(halfSizeY);
-        mActionDetailsPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
-                                          Short.MAX_VALUE));
-        
-        mTopPanel.setMinimumSize(halfSizeY);
-        mTopPanel.setPreferredSize(new Dimension(Short.MAX_VALUE,
-                Short.MAX_VALUE));
-        mTopPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
-                                          Short.MAX_VALUE));
+       /// mTopPanel.setMinimumSize(halfSizeY);
+       /// mTopPanel.setPreferredSize(new Dimension(Short.MAX_VALUE,
+       ///         Short.MAX_VALUE));
+       /// mTopPanel.setMaximumSize(new Dimension(Short.MAX_VALUE,
+        ///                                  Short.MAX_VALUE));
         /*
         mActionPanel.setMinimumSize(halfSizeBoth);
         mActionPanel.setPreferredSize(halfSizeBoth);
@@ -192,9 +154,9 @@ public class MainWindow {
         mButtonListPanel.setMaximumSize(halfSizeBoth);
         */
         mButtonScrollPane.setMinimumSize(halfSizeBoth);
-        mButtonScrollPane.setPreferredSize(halfSizeBoth);
-        mButtonScrollPane.setMaximumSize(new Dimension(Short.MAX_VALUE,
-                                          Short.MAX_VALUE));
+        //mButtonScrollPane.setPreferredSize(halfSizeBoth);
+        //mButtonScrollPane.setMaximumSize(new Dimension(Short.MAX_VALUE,
+         //                                 Short.MAX_VALUE));
 /*
         mButtonPanel.setMinimumSize(halfSizeBoth);
         mButtonPanel.setPreferredSize(halfSizeBoth);
@@ -211,9 +173,9 @@ public class MainWindow {
                                          */
         
         mActionScrollPane.setMinimumSize(halfSizeBoth);
-        mActionScrollPane.setPreferredSize(halfSizeBoth);
-        mActionScrollPane.setMaximumSize(new Dimension(Short.MAX_VALUE,
-                                          Short.MAX_VALUE));
+        //mActionScrollPane.setPreferredSize(halfSizeBoth);
+       // mActionScrollPane.setMaximumSize(new Dimension(Short.MAX_VALUE,
+        //                                  Short.MAX_VALUE));
     	
     	mMainWindowFrame.pack();
     	mMainWindowFrame.setVisible(true);
@@ -249,6 +211,7 @@ public class MainWindow {
     	// If the component does not already exist as a child,
     	// set it to be a child, and set it as visible/enabled.
     	if(!found) {
+    		
     		mActionDetailsPanel.add(panel);
     		panel.setVisible(true);
     		panel.setEnabled(true);
@@ -256,5 +219,6 @@ public class MainWindow {
     	
     	// Restore validity.
     	mMainWindowFrame.validate();
+    	mMainWindowFrame.repaint(50L);
     }
 }
