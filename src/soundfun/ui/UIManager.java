369c3d3a7cde908adecb7b0512e35088af521514
Denny Kunker <wkunker@gmail.com>
2013-03-31 17:38:14 -0400

    Updated .gitignore to ignore the ant-generated soundfun.jar if ant clean hasn't been run before a git add. Updated the prerequisites in README. Updated Ant build script.

--- src/soundfun/ui/UIManager.java
+++ src/soundfun/ui/UIManager.java
@@ -35,18 +35,8 @@ public class UIManager {
 	/*
 	 * Returns the main JFrame instance, reloads the main window.
 	 * Note: This is not a visually "seamless" reload.
-	 * @param panel the javax.swing.JPanel to be loaded in.
 	 */
 	public MainWindow reloadMainWindow() {
-		/*
-		 * TODO: ActionDetailsPanel currently must be injected into the
-		 * constructor of the main window class. This is a huge
-		 * limitation because the whole window has to be reloaded
-		 * when a different plugin is selected for viewing. It's
-		 * done like this for now to make NetBeans prototyping easy,
-		 * but this will have to change in the future, in favor
-		 * of a more permanent solution (ie inject through a method).
-		 */
 		if(mMainWindow != null) { // Dispose old instance if it exists.
 			mMainWindow = null;
 		}
